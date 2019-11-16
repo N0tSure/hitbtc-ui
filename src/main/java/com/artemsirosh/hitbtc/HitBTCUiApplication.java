12e0bffc34dfaa50c377c9e430cb6244551d2c75
N0tSure <ASir2089@gmail.com>
2019-11-16 16:46:43 +0300

    PE0024 Initial commit

---
+++ src/main/java/com/artemsirosh/hitbtc/HitBTCUiApplication.java
@@ -0,0 +1,13 @@
+package com.artemsirosh.hitbtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitBTCUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitBTCUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemsirosh/hitbtc/HitBTCUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemsirosh.hitbtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitBTCUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
