8d5e5c77a428cf87d2c6e2bc823e15fe3b152846
Artem Sirosh <artem.sirosh@t-systems.com>
2019-11-21 19:34:47 +0300

    PE0025 Move symbol view to separate component
    Symbols view moved to separated component and MainView changed to
    MainLayout: main layout for a views.

---
+++ src/main/java/com/artemsirosh/hitbtc/view/MainLayout.java
@@ -0,0 +1,68 @@
+package com.artemsirosh.hitbtc.view;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.HasElement;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Consumer;
+
+/**
+ * Created on 17 Nov, 2019.
+ *
+ * This is layout for all view on HitBTC UI. Contains navigations, header and footer.
+ *
+ * @author Artemis A. Sirosh
+ */
+@Slf4j
+@UIScope
+@SpringComponent
+public class MainLayout extends Composite<VerticalLayout> implements RouterLayout {
+
+    private final Consumer<HasElement> contentRegisterer;
+    private final Consumer<HasElement> contentUnRegisterer;
+
+    public MainLayout() {
+        final HorizontalLayout header = new HorizontalLayout();
+        header.setWidth("100%");
+        header.setPadding(true);
+        header.add(new H1("HitBTC UI"));
+
+        final VerticalLayout navBar = new VerticalLayout();
+        navBar.setWidth("200px");
+
+        final HorizontalLayout center = new HorizontalLayout();
+        center.setWidth("100%");
+        center.setFlexGrow(1, navBar);
+        center.add(navBar);
+
+        this.contentRegisterer = hasElement -> center.getElement().appendChild(hasElement.getElement());
+        this.contentUnRegisterer = hasElement -> center.getElement().removeChild(hasElement.getElement());
+
+        final HorizontalLayout footer = new HorizontalLayout();
+        footer.setWidth("100%");
+        footer.setPadding(true);
+
+        getContent().setSizeFull();
+        getContent().setPadding(false);
+        getContent().setSpacing(false);
+        getContent().add(header, center, footer);
+        getContent().expand(center);
+
+    }
+
+    @Override
+    public void showRouterLayoutContent(HasElement content) {
+        this.contentRegisterer.accept(content);
+    }
+
+    @Override
+    public void removeRouterLayoutContent(HasElement oldContent) {
+        this.contentUnRegisterer.accept(oldContent);
+    }
+}
---
+++ src/main/java/com/artemsirosh/hitbtc/view/SymbolsView.java
@@ -0,0 +1,58 @@
+package com.artemsirosh.hitbtc.view;
+
+import com.artemsirosh.hitbtc.model.Symbol;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.provider.Query;
+import com.vaadin.flow.function.SerializablePredicate;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+
+import java.util.Optional;
+
+/**
+ * Created at 21-11-2019
+ *
+ * This view contains a table with available {@link Symbol}s.
+ *
+ * @author Artem Sirosh 'Artem.Sirosh@t-systems.com'
+ */
+@Tag("div")
+@Route(layout = MainLayout.class, value = "")
+@UIScope
+@SpringComponent
+public class SymbolsView extends Composite<HorizontalLayout> {
+
+    public SymbolsView(ListDataProvider<Symbol> symbolListDataProvider, SymbolDetails symbolDetails) {
+
+        final ComboBox<String> filterSelect = new ComboBox<>("Find symbol");
+        filterSelect.setItems(symbolListDataProvider.fetch(new Query<>()).map(Symbol::getId));
+        filterSelect.setClearButtonVisible(true);
+        filterSelect.addValueChangeListener(event -> {
+            final SerializablePredicate<Symbol> filter = Optional.ofNullable(event.getValue())
+                .map(id -> (SerializablePredicate<Symbol>) (symbol -> symbol.getId().startsWith(id)))
+                .orElse(symbol -> true);
+            symbolListDataProvider.setFilter(filter);
+        });
+
+        final Grid<Symbol> grid = new Grid<>(Symbol.class);
+
+        grid.setColumns();
+        grid.addColumn(Symbol::getId).setComparator(Symbol::getId).setHeader(filterSelect);
+        grid.addColumn(Symbol::getBaseCurrency).setComparator(Symbol::getBaseCurrency).setHeader("Base Currency");
+        grid.addColumn(Symbol::getQuoteCurrency).setComparator(Symbol::getQuoteCurrency).setHeader("Quote Currency");
+
+        grid.setDataProvider(symbolListDataProvider);
+        grid.asSingleSelect().addValueChangeListener(event -> symbolDetails.update(event.getValue()));
+
+        this.getContent().setWidth("100%");
+        this.getContent().add(new VerticalLayout(new H2("Symbols"), grid), symbolDetails);
+    }
+}
