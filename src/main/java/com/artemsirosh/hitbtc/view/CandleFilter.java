c64787940d54596855e1cc85ea5d18bfb33d85ee
N0tSure <ASir2089@gmail.com>
2020-04-04 17:02:21 +0300

    PE0025 Update filter for Candles

--- src/main/java/com/artemsirosh/hitbtc/view/CandleFilter.java
+++ src/main/java/com/artemsirosh/hitbtc/view/CandleFilter.java
@@ -5,9 +5,17 @@ import lombok.*;
 
 import java.time.Instant;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 /**
  * Created on 04 Jan, 2020.
+ *
+ * Allows to filter candle by:
+ * <ul>
+ *     <li>{@link CandleFilter#period} -- candle period</li>
+ *     <li>{@link CandleFilter#from} -- since date-time</li>
+ *     <li>{@link CandleFilter#till} -- date-time until</li>
+ * </ul>
  *
  * @author Artemis A. Sirosh
  */
@@ -15,11 +23,16 @@ import java.util.Optional;
 @Value
 public class CandleFilter {
 
-    private Period period;
-    private Instant from;
-    private Instant till;
+    public final static BiFunction<CandleFilter, Period, CandleFilter> periodModifier =
+        (filter, value) -> new CandleFilter(value, filter.from, filter.till);
 
-    public Optional<Period> getPeriod() {
-        return Optional.ofNullable(period);
-    }
+    public final static BiFunction<CandleFilter, Instant, CandleFilter> fromDTModifier =
+        (filter, value) -> new CandleFilter(filter.period, value, filter.till);
+
+    public final static BiFunction<CandleFilter, Instant, CandleFilter> tillDTModifier =
+        (filter, value) -> new CandleFilter(filter.period, filter.from, value);
+
+    Period period;
+    Instant from;
+    Instant till;
 }
