9ef5d55dbc5b9b9285e474a96949e2956d37e470
N0tSure <ASir2089@gmail.com>
2020-04-07 01:24:35 +0300

    PE0025 Update Period

--- src/main/java/com/artemsirosh/hitbtc/model/Period.java
+++ src/main/java/com/artemsirosh/hitbtc/model/Period.java
@@ -3,7 +3,19 @@ package com.artemsirosh.hitbtc.model;
 /**
  * Created on 24 Nov, 2019.
  *
- * Temporal periods.
+ * Temporal periods. This is the next periods:
+ * <ul>
+ *     <li>M1 -- one minute</li>
+ *     <li>M3 -- three minutes</li>
+ *     <li>M5 -- five minutes</li>
+ *     <li>M15 -- fifteen minutes</li>
+ *     <li>M30 -- half hour</li>
+ *     <li>H1 -- one hour</li>
+ *     <li>H4 -- four hour</li>
+ *     <li>D1 -- one hour</li>
+ *     <li>D7 -- one week</li>
+ *     <li>1M -- one month</li>
+ * </ul>
  *
  * @author Artemis A. Sirosh
  */
