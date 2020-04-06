4c0c8e8f9c7775b1d64bf2ce8ddcd8b1be2eaeab
N0tSure <ASir2089@gmail.com>
2020-04-07 00:29:48 +0300

    PE0025 Update CandlesView
    CandlesView can filter record by period, can filter by lower and
    upper bound timestamp. Also, sorting by timestamp enabled.

--- src/main/java/com/artemsirosh/hitbtc/view/CandlesView.java
+++ src/main/java/com/artemsirosh/hitbtc/view/CandlesView.java
@@ -1,12 +1,11 @@
 package com.artemsirosh.hitbtc.view;
 
-import com.artemsirosh.hitbtc.client.CandleClient;
 import com.artemsirosh.hitbtc.model.Candle;
 import com.artemsirosh.hitbtc.model.Period;
-import com.artemsirosh.hitbtc.model.SortDirection;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -21,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.*;
-import java.util.stream.Stream;
 
 /**
  * Created at 21-11-2019
@@ -36,79 +34,77 @@ import java.util.stream.Stream;
 public class CandlesView extends Composite<VerticalLayout> implements HasUrlParameter<String> {
 
     private final Consumer<String> symbolParamProcessor;
-    private final CandleClient candleClient;
+    private final ConfigurableFilterDataProvider<Candle, Void, CandleFilter> provider;
 
     private String symbol;
+    private CandleFilter candleFilter;
 
-    public CandlesView(CandleClient candleClient, InstantPicker fromInstantPicker, InstantPicker tillInstantPicker) {
-        this.candleClient = candleClient;
-        this.symbolParamProcessor = s -> {};
+    public CandlesView(
+        BiFunction<Query<Candle, CandleFilter>, String, List<Candle>> candleDataProvider,
+        InstantPicker fromInstantPicker, InstantPicker tillInstantPicker
+    ) {
+
+        final H3 symbolName = new H3();
+        final Consumer<String> symbolNameUpdater = symbolName::setText;
+        this.symbolParamProcessor = symbolNameUpdater.andThen(s -> this.symbol = s);
 
         final ComboBox<Period> periodsSelect = new ComboBox<>();
         periodsSelect.setItems(Period.values());
-        periodsSelect.setValue(Period.M30);
+        periodsSelect.setValue(Period.MO);
         periodsSelect.setLabel("Time periods");
+        periodsSelect.addValueChangeListener(event -> setCandleFilter(CandleFilter.periodModifier, event.getValue()));
+
+        fromInstantPicker.addValueChangeListener(tillInstantPicker.getListener());
+        fromInstantPicker.addValueChangeListener(
+            event -> setCandleFilter(CandleFilter.fromDTModifier, event.getValue())
+        );
+
+        tillInstantPicker.addValueChangeListener(fromInstantPicker.getListener());
+        tillInstantPicker.addValueChangeListener(
+            event -> setCandleFilter(CandleFilter.tillDTModifier, event.getValue())
+        );
 
         final HorizontalLayout filters = new HorizontalLayout();
         filters.add(periodsSelect, fromInstantPicker, tillInstantPicker);
 
-//        final Grid<Candle> grid = new Grid<>(Candle.class);
-//        grid.setColumns();
-//        grid.addColumn(Candle::getTimestamp).setHeader("Opening Date");
-//        grid.addColumn(Candle::getOpen).setHeader("Opening Price");
-//        grid.addColumn(Candle::getClose).setHeader("Closing Price");
-//        grid.addColumn(Candle::getMax).setHeader("Maximum Price");
-//        grid.addColumn(Candle::getMin).setHeader("Minimum Price");
-//        grid.addColumn(Candle::getVolume).setHeader("Base Currency Volume");
-//        grid.addColumn(Candle::getVolumeQuote).setHeader("Quote Currency Volume");
-
-        final BiFunction<Query<Candle, CandleFilter>, CandleFilter, List<Candle>> filterCandles = (query, filter) ->
-            candleClient.getCandles(
-                symbol,
-                filter.getPeriod().orElse(Period.MO),
-                query.getSortOrders().stream()
-                    .map(QuerySortOrder::getDirection)
-                    .map(SortDirection.modelConverter)
-                    .findFirst()
-                    .orElse(SortDirection.ASC),
-                filter.getFrom(),
-                filter.getTill(),
-                query.getLimit(),
-                query.getOffset()
-            );
-
-        final Function<Query<Candle, CandleFilter>, List<Candle>> getCandles = query -> candleClient.getCandles(
-            symbol,
-            Period.MO,
-            SortDirection.ASC,
-            null,
-            null,
-            query.getLimit(),
-            query.getOffset()
-        );
-
-        final Function<Query<Candle, CandleFilter>, List<Candle>> queryCandlesByFilter = query ->
-            query.getFilter()
-                .map(filter -> filterCandles.apply(query, filter))
-                .orElse(getCandles.apply(query));
-
-        final IntBinaryOperator pageSizeEvaluator = Math::max;
 
         final CallbackDataProvider<Candle, CandleFilter> callbackDataProvider = DataProvider.fromFilteringCallbacks(
-            query -> queryCandlesByFilter.apply(query).stream(),
-            query -> pageSizeEvaluator.applyAsInt(queryCandlesByFilter.apply(query).size(), query.getLimit())
+            query -> candleDataProvider.apply(loggingAround(query), symbol).stream(),
+            query -> candleDataProvider.apply(query, symbol).size()
         );
-        final ConfigurableFilterDataProvider<Candle, Void, CandleFilter> provider =
-            callbackDataProvider.withConfigurableFilter();
-//        filterDataProvider.setFilter();
 
+        provider = callbackDataProvider.withConfigurableFilter();
 
+        final Grid<Candle> grid = new Grid<>(Candle.class);
+        grid.setColumns();
+        grid.addColumn(Candle::getTimestamp).setKey("ts").setSortable(true).setHeader("Opening Date");
+        grid.addColumn(Candle::getOpen).setHeader("Opening Price");
+        grid.addColumn(Candle::getClose).setHeader("Closing Price");
+        grid.addColumn(Candle::getMax).setHeader("Maximum Price");
+        grid.addColumn(Candle::getMin).setHeader("Minimum Price");
+        grid.addColumn(Candle::getVolume).setHeader("Base Currency Volume");
+        grid.addColumn(Candle::getVolumeQuote).setHeader("Quote Currency Volume");
+        grid.setDataProvider(provider);
+        grid.setPageSize(100);
 
-        fromInstantPicker.addValueChangeListener(event -> new )
+        this.getContent().add(symbolName, filters, grid);
+    }
 
-        final H3 symbolName = new H3();
-        this.getContent().add(symbolName, filters);
+    private <T> void setCandleFilter(BiFunction<CandleFilter, T, CandleFilter> filterValueChanger, T value) {
+        final CandleFilter filter = Optional.ofNullable(this.candleFilter)
+            .orElseGet(() -> new CandleFilter(null, null, null));
 
+        this.candleFilter = filterValueChanger.apply(filter, value);
+        this.provider.setFilter(this.candleFilter);
+        log.info("Update value: {}", value);
+    }
+
+    private Query<Candle, CandleFilter> loggingAround(Query<Candle, CandleFilter> query) {
+        log.info(
+            "Query offset: {}, limit: {}, sorts: {} and filter {}",
+            query.getOffset(), query.getLimit(), query.getSortOrders(), query.getFilter()
+        );
+        return query;
     }
 
     @Override
@@ -116,4 +112,5 @@ public class CandlesView extends Composite<VerticalLayout> implements HasUrlPara
         Optional.ofNullable(parameter)
             .ifPresent(symbolParamProcessor);
     }
+
 }
