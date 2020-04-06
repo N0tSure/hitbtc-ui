a7405c9af208e7d41edaca13c0b0919419846b27
N0tSure <ASir2089@gmail.com>
2020-04-06 23:50:19 +0300

    PE0025 Update CandleClient

--- src/main/java/com/artemsirosh/hitbtc/client/CandleClient.java
+++ src/main/java/com/artemsirosh/hitbtc/client/CandleClient.java
@@ -1,7 +1,6 @@
 package com.artemsirosh.hitbtc.client;
 
 import com.artemsirosh.hitbtc.model.Candle;
-import com.artemsirosh.hitbtc.model.Period;
 import com.artemsirosh.hitbtc.model.SortDirection;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +13,25 @@ import java.util.List;
 /**
  * Created on 24 Nov, 2019.
  *
+ * Allows to retrieve {@link Candle}s from HitBTC.
+ *
  * @author Artemis A. Sirosh
  */
 @FeignClient(name = "candles", url = "https://api.hitbtc.com/api/2/public/candles")
 public interface CandleClient {
 
+    /**
+     * Makes query for {@link Candle}s to HitBTC API.
+     *
+     * @param symbol the {@link com.artemsirosh.hitbtc.model.Symbol} identifier
+     * @param period of time for a candle
+     * @param sortDirection direction of record sorting
+     * @param from timestamp of lower query bound
+     * @param till timestamp of upper query bound
+     * @param limit amount of records requested
+     * @param offset of records query
+     * @return a list of {@link Candle}
+     */
     @GetMapping("/{symbol}")
     List<Candle> getCandles(
         @PathVariable("symbol") String symbol,
